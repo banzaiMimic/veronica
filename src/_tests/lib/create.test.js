36beaf3aed8826a753a032a00a4dfa8f28229fd0
dr-mimic <gartflores@gmail.com>
2021-07-02 13:47:50 -0400

    all core files added to endpoint template

--- src/_tests/lib/create.test.js
+++ src/_tests/lib/create.test.js
@@ -30,18 +30,6 @@ test('getFilenameUpdate returns correct filename for useCasese', () => {
   expect(filenameUpdate).toBe('useCases/actCart.js')
 })
 
-test('getFilenameUpdate returns correct filename with blank action', () => {
-  let filenameUpdate = makeCreate({
-    ...createDefaults,
-    replacements: {
-      method: 'post',
-      action: '',
-      entity: 'booking'
-    }
-  }).getFilenameUpdate({filename: 'controllers/[method][action][entity].js'})
-  expect(filenameUpdate).toBe('controllers/postBooking.js')
-})
-
 test('getFilenameUpdate returns correct filename for [entity].js', () => {
   let filenameUpdate = makeCreate({
     ...createDefaults,
